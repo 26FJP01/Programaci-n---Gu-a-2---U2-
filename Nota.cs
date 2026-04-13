cb0ad77834dbdfd66f8ed91db0e470baed5d4907
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-04-13 17:48:07 -0600

    Update Nota.cs

--- Nota.cs
+++ Nota.cs
@@ -3,7 +3,6 @@
     private static void Main(string[] args)
     {
         Console.Write("DIME LA NOTA QUE OBTUVISTE EN LETRAS: ");
-        //variable
         string nota;
         nota = Console.ReadLine();
         switch (nota)
