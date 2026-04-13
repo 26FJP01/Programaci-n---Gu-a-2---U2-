eb40101fc6042c60bef2c04a173538e206c6a6b7
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-04-13 17:55:42 -0600

    Add files via upload

---
+++ MenuComida.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(" MENU DEL DÍA");
+        Console.WriteLine("1. HAMBURGUESA");
+        Console.WriteLine("2. PIZZA");
+        Console.WriteLine("3. POLLO FRITO");
+        Console.WriteLine("4. ENSALADA");
+        int opc;
+        Console.Write("Digite la Opción: []");
+        opc = Convert.ToInt32(Console.ReadLine());
+        switch (opc)
+        {
+            case 1:
+                Console.WriteLine("Pedido: HAMBURGUESA");
+                break;
+            case 2:
+                Console.WriteLine("Pedido: PIZZA");
+                break;
+            case 3:
+                Console.WriteLine("Pedido: POLLO FRITO");
+                break;
+            case 4:
+                Console.WriteLine("Pedido: ENSALADA");
+                break;
+            default:
+                Console.WriteLine("Número Inválido; Digite una opción válida");
+                break;
+        }
+    }
+}
---
+++ MenuComida.psc
@@ -0,0 +1,22 @@
+Algoritmo MenuComida
+	Definir opc Como Entero
+	Escribir "    MENU DEL DÍA"
+	Escribir "1- HAMBURGUESA"
+	Escribir "2- PIZZA"
+	Escribir "3- POLLO FRITO"
+	Escribir "4- ENSALADA"
+	Escribir Sin Saltar "Digite la Opción: []"
+	Leer opc
+	Segun opc Hacer
+		1:
+			Escribir "Pedido: HAMBURGUESA"
+		2:
+			Escribir "Pedido: PIZZA"
+		3:
+			Escribir "Pedido: POLLO FRITO"
+		4:
+			Escribir "Pedido: ENSALADA"
+		De Otro Modo:
+			Escribir "Número Inválido; Digite una opción válida"
+	Fin Segun
+FinAlgoritmo
