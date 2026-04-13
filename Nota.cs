07d845a6151d429455f70e4fed9a01ce5d27e586
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-04-13 17:46:50 -0600

    Add files via upload

---
+++ Nota.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("DIME LA NOTA QUE OBTUVISTE EN LETRAS: ");
+        //variable
+        string nota;
+        nota = Console.ReadLine();
+        switch (nota)
+        {
+            case "A":
+                Console.WriteLine("EXCELENTE");
+                break;
+            case "B":
+                Console.WriteLine("BUENO");
+                break;
+            case "C":
+                Console.WriteLine("REGULAR");
+                break;
+            case "D":
+                Console.WriteLine("DEFICIENTE");
+                break;
+            case "F":
+                Console.WriteLine("REPROBADO");
+                break;
+            default:
+                Console.WriteLine("LETRA INVALIDA!!!");
+                break;
+        }
+    }
+}
---
+++ Notas.psc
@@ -0,0 +1,19 @@
+Algoritmo Notas
+	Definir nota Como Caracter
+	Escribir Sin Saltar "DIME LA NOTA  QUE OBTUVISTE EN LETRAS"
+	Leer nota
+	Segun nota Hacer
+		"A":
+			Escribir "EXCELENTE"
+		"B":
+			Escribir "BUENO"
+		"C":
+			Escribir "REGULAR"
+		"D":
+			Escribir "DEFICIENTE"
+		"F":
+			Escribir "REPROBADO"
+		De Otro Modo:
+			Escribir "LETRA INVALIDA!!!"
+	Fin Segun
+FinAlgoritmo
