8952623c09cd4ea3d2edb25d5af964d7c953fbbb
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-04-13 18:12:07 -0600

    Add files via upload

---
+++ Mese.cs
@@ -0,0 +1,52 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("MESES DEL AÑO");
+        int mes;
+        Console.Write("Dime un número del 1 al 12: ");
+        mes = Convert.ToInt32(Console.ReadLine());
+        switch (mes)
+        {
+            case 1:
+                Console.WriteLine("ENERO:31 días");
+                break;
+            case 2:
+                Console.WriteLine("FEBRERO:28 (Si es año bisiesto 29) días");
+                break;
+            case 3:
+                Console.WriteLine("MARZO:31 días");
+                break;
+            case 4:
+                Console.WriteLine("ABRIL:30 días");
+                break;
+            case 5:
+                Console.WriteLine("MAYO:31 días");
+                break;
+            case 6:
+                Console.WriteLine("JUNIO:30 días");
+                break;
+            case 7:
+                Console.WriteLine("JULIO:31 días");
+                break;
+            case 8:
+                Console.WriteLine("AGOSTO:31 días");
+                break;
+            case 9:
+                Console.WriteLine("SEPTIEMBRE:30 días");
+                break;
+            case 10:
+                Console.WriteLine("OCTUBRE:31 días");
+                break;
+            case 11:
+                Console.WriteLine("NOVIEMBRE:30 días");
+                break;
+            case 12:
+                Console.WriteLine("DICIEMBRE:31 días");
+                break;
+            default:
+                Console.WriteLine("DIGITE UN NUMERO ENTRE 1 Y EL 12");
+                break;
+        }
+    }
+}
---
+++ Meses.psc
@@ -0,0 +1,33 @@
+Algoritmo Meses
+	Definir mes Como Entero
+	Escribir Sin Saltar "Dime un número del 1 al 12: "
+	Leer mes
+	Segun mes Hacer
+		1:
+			Escribir "ENERO: 31 días"
+		2:
+			Escribir "FEBRERO: 28 (Si es año bisiesto 29)días"
+		3:
+			Escribir "MARZO: 31d días"
+		4:
+			Escribir "ABRIL: 30 días"
+		5: 
+			Escribir "MAYO: 31 días"
+		6: 
+			Escribir "JUNIO: 30 días"
+		7:
+			Escribir "JULIO: 31 días"
+		8:
+			Escribir "AGOSTO: 31 días"
+		9: 
+			Escribir "SEPTIEMBRE: 30 días"
+		10:
+			Escribir "OCTUBRE: 31 días"
+		11:	
+			Escribir "NOVIEMBRE: 30 días"
+		12:
+			Escribir "DICIEMBRE: 31 días"
+		De Otro Modo:
+			Escribir "DIGITE UN NUMERO ENTRE EL 1 Y EL 12"
+	Fin Segun
+FinAlgoritmo
