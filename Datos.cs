0503459a17aecca564c6eed1d5fdda3b4198b957
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-04-13 18:32:38 -0600

    Add files via upload

---
+++ Datos.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        char caracte;
+        Console.Write("Ingresa un Carácter (Vocal o Digito): ");
+        caracte = Convert.ToChar(Console.ReadLine());
+        switch (caracte)
+        {
+            case 'a':
+            case 'A':
+            case 'e':
+            case 'E':
+            case 'i':
+            case 'I':
+            case 'o':
+            case 'O':
+            case 'u':
+            case 'U':
+                Console.WriteLine("ES UNA VOCAL");
+                break;
+            default:
+                if (caracte >= '1' && caracte <= '9')
+                    Console.WriteLine("ES UN DIGITO");
+                else
+                    Console.WriteLine("NO ES DIGITO NI VOCAL");
+                break;
+        }
+    }
+}
---
+++ Datos.psc
@@ -0,0 +1,13 @@
+Algoritmo Datos
+	Definir caracte Como Caracter
+	Escribir "Ingresa un carácter (Vocal o Digito): "
+	Leer Caracte
+	Segun caracte Hacer
+		"a" O "e" O "i" O "o" O "u":
+			Escribir "ES UNA VOCAL"
+		"0" O "1" O "2" O "3" O "4" O "5" O "6" O "7" O "8" O "9":
+			Escribir "ES UN DIGITO"
+		De Otro Modo:
+			Escribir "NO ES VOCAL NI DIGITO"
+	Fin Segun
+FinAlgoritmo
